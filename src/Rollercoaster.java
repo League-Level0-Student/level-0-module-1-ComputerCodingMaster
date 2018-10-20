637729fc18cb16e84dbf746de5e2dd71ed83bd48
ComputerCodingMaster <league@WorthyMini.local>
2018-10-20 11:26:56 -0700

    i am doing rollercoaster and finished 2 assignments in module 1 and 2
    i got animal crackers

---
+++ src/Rollercoaster.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Rollercoaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String mymy = JOptionPane.showInputDialog("What height are you?");
+	
+	}
+
+
+}
