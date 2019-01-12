ff9b66b9eda9a5120886d0f3033ac07354a678c7
ComputerCodingMaster <league@Mac-mini-4.local>
2019-01-12 11:16:39 -0800

    I'm still doing obedient robot
    4headOmegalul

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -6,7 +6,7 @@ static Robot bob;
 		// TODO Auto-generated method stub
 		bob = new Robot();
 	drawSquare();
-	
+	drawTriangle();
 	}
 
 	static void drawSquare() {
@@ -21,4 +21,18 @@ static Robot bob;
 		bob.turn(90);
 		bob.move(100);
 	}
+	static void drawTriangle() {
+		bob.hide();
+		bob.setPenColor(255,0,0);
+		bob.turn(45);
+		bob.move(100);
+		bob.turn(90);
+		bob.move(100);
+		bob.turn(135);
+		bob.move(100);
+
+
+
+	}
+
 }
