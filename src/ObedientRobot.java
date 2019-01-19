3e6a70127763b7824a7f255f48a4d0bf729fff1f
EmperorlNeo <league@Admins-Mac-mini-2.local>
2019-01-19 11:28:07 -0800

    almost done
    yeetery

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
@@ -5,13 +7,13 @@ static Robot bob;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		bob = new Robot();
-	drawSquare();
-	drawTriangle();
-	}
+		JOptionPane.showInputDialog(null,"What shape do you want draw?");
 
+	}
 	static void drawSquare() {
 		bob.hide();
 		bob.setPenColor(255, 0, 0);
+		bob.setSpeed(1000);
 		bob.penDown();
 		bob.move(100);
 		bob.turn(90);
@@ -24,15 +26,21 @@ static Robot bob;
 	static void drawTriangle() {
 		bob.hide();
 		bob.setPenColor(255,0,0);
+		bob.setSpeed(1000);
 		bob.turn(45);
 		bob.move(100);
 		bob.turn(90);
 		bob.move(100);
 		bob.turn(135);
 		bob.move(100);
-
-
-
 	}
-
-}
+	static void drawCircle() {
+		bob.hide();
+		bob.setPenColor(255,0,0);
+		bob.setSpeed(1000);
+		for(int i=1; i<100; i++) {
+		bob.turn(5);
+		bob.move(1);
+	}
+	}
+	}
