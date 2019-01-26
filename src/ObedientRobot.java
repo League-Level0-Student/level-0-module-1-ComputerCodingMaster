3341013a7bd74552cf1193b5ac3fdb8f5f9a96f3
ComputerCodingMaster <league@Mac-mini-3.local>
2019-01-26 11:11:30 -0800

    I'm done with that
    ailmao

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -3,16 +3,32 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-static Robot bob;
+	static Robot bob;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		bob = new Robot();
-		JOptionPane.showInputDialog(null,"What shape do you want draw?");
+		String color = JOptionPane.showInputDialog(null, "What color do you want? Red,Blue,or Green?");
+		if (color.equalsIgnoreCase("Red")) {
+			drawRed();
+		} else if (color.equalsIgnoreCase("Blue")) {
+			drawBlue();
+		} else if (color.equalsIgnoreCase("Green")) {
+			drawGreen();
+		}
+		String shape = JOptionPane.showInputDialog(null, "Do you want to draw a circle,a square,or a triangle?");
+		if (shape.equalsIgnoreCase("Square")) {
+			drawSquare();
+		} else if (shape.equalsIgnoreCase("Triangle")) {
+			drawTriangle();
+		} else if (shape.equalsIgnoreCase("Circle")) {
+			drawCircle();
+		}
 
 	}
+
 	static void drawSquare() {
 		bob.hide();
-		bob.setPenColor(255, 0, 0);
 		bob.setSpeed(1000);
 		bob.penDown();
 		bob.move(100);
@@ -23,24 +39,40 @@ static Robot bob;
 		bob.turn(90);
 		bob.move(100);
 	}
+
 	static void drawTriangle() {
 		bob.hide();
-		bob.setPenColor(255,0,0);
 		bob.setSpeed(1000);
+		bob.penDown();
 		bob.turn(45);
 		bob.move(100);
 		bob.turn(90);
 		bob.move(100);
 		bob.turn(135);
-		bob.move(100);
+		bob.move(140);
 	}
+
 	static void drawCircle() {
 		bob.hide();
-		bob.setPenColor(255,0,0);
+		bob.penDown();
 		bob.setSpeed(1000);
-		for(int i=1; i<100; i++) {
-		bob.turn(5);
-		bob.move(1);
+		for (int i = 1; i < 100; i++) {
+			bob.turn(5);
+			bob.move(1);
+
+		}
+
 	}
+
+	static void drawRed() {
+		bob.setPenColor(255, 0, 0);
 	}
+
+	static void drawBlue() {
+		bob.setPenColor(0, 0, 255);
 	}
+
+	static void drawGreen() {
+		bob.setPenColor(0, 255, 0);
+	}
+}
